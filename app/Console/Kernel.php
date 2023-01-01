04808c2802fd6cc3881425fe6d0895952c297442
Ar Kar Bo <eia99fia99@gmail.com>
2023-01-01 23:29:19 +0630

    update schedule time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('users:logged')->dailyAt("23:30");
+        $schedule->command('users:logged')->dailyAt("23:33");
     }
 
     /**
